1e1f8d1786867bb105cbecec29e1f011493e4cef
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2024-12-12 14:55:13 +0200

    Added SearchBar styles in styles/searchBar.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
 import React, { useState } from 'react';
 import CurrentWeather from './components/CurrentWeather';
+import SearchBar from './components/SearchBar';
+import WeatherGrid from './components/WeatherGrid';
+import './styles/components/a11y.css';
 
 const App = () => {
-  const [city, setCity] = useState('New York');
+  const [city, setCity] = useState('');
 
-  const handleCityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCity(event.target.value);
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleSearch = (city: string) => {
+    setCity(city);
   };
 
   return (
     <div className="app-container">
-      <h1>Weather App</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="city-input">Enter city name:</label>
-        <input
-          type="text"
-          id="city-input"
-          value={city}
-          onChange={handleCityChange}
-          placeholder="Enter city name"
-          className="city-input"
-        />
-        <button type="submit" className="search-button">Search</button>
-      </form>
-      <CurrentWeather city={city} />
+      <h1 className='offscreen'>Responsive Weather App</h1>
+      <SearchBar onSearch={handleSearch} />
+      <a href="#CurrentWeather" className='skip-link'>Skip to Current Conditions</a>
+      <CurrentWeather city={city}/>
+      <WeatherGrid />
+      <p id='confirmation' className='hidden' aria-live='assertive'></p>
     </div>
   );
 };
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { TbCloudSearch } from "react-icons/tb";
+import { SiApachecloudstack } from "react-icons/si";
+import '../styles/components/a11y.css';
+import '../styles/components/searchBar.css'
 
 type SearchBarProps = {
   onSearch: (city: string) => void;
@@ -17,15 +21,21 @@ const SearchBar = ({ onSearch }: SearchBarProps) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} role="search" className='search-form'>
+      <label className="offscreen" htmlFor="search">Enter a city name:</label>
       <input
         id="search"
         type="text"
         value={city}
         onChange={handleInputChange}
         placeholder="Enter city name"
+        className="focus:outline-none pl-3"
+        role="searchbox"
       />
-      <button type="submit">Search</button>
+      <button type="submit" className="search-button" title="Lookup City" aria-label="Submit Search">
+        <TbCloudSearch className="button1 stroke-[1.5]" size={40} />
+        <SiApachecloudstack className='button2 hidden stroke-[0.19px]' size={40}/>
+      </button>
     </form>
   );
 };
