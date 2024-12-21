e3f2706d2bfb895edf573030016ad2165048aac3
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2024-12-21 22:41:07 +0200

    Updated light dark mode

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -34,7 +34,7 @@ const CurrentWeather = ({ weatherData, error }: CurrentWeatherProps) => {
   return (
     <div className="current-display bg-slate-50 bg-opacity-25 p-3 rounded-xl shadow-lg max-w-lg mx-auto mb-3 transition-all duration-500 ease-in-out transform" role="region" aria-labelledby="CurrentWeather">
       
-      <h2 id="CurrentWeather" className="font-bold text-center mb-6">{name}</h2>
+      <h2 id="CurrentWeather" className="font-bold text-2xl text-center mb-6">{name}</h2>
 
       <div className="flex justify-center items-center space-x-11 mb-6">
         {/* Temperature */}
