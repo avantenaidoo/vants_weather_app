b0999c4171d005815ec95451a010cdc0b838acfa
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2024-12-25 23:03:36 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,6 @@ const cachesName = 'weather-app-cache-v1';
 const urlsToCache = [
   '/vants_weather_app/',             
   '/vants_weather_app/index.html',   
-  '/vants_weather_app/assets/index.css',
   '/vants_weather_app/images/favicon.jpeg',
   '/vants_weather_app/images/clouds.png',  
   // Add other assets if needed
