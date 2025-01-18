c9605bb6f3cf5d4f6f70b203aa437e4e0443c660
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-01-18 10:11:15 +0200

    Fix weatherStackUrl

--- backend/server/src/config/config.ts
+++ backend/server/src/config/config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 export const config = {
     port: process.env.PORT || 3000,
-    weathStackUrl: process.env.WEATHERSTACK_URL,
+    weatherStackUrl: process.env.WEATHERSTACK_URL,
     weatherStackApiKey: process.env.WEATHERSTACK_API_KEY,
     visualCrossingUrl: process.env.VISUALCROSSING_URL,
     visualCrossingApiKey: process.env.VISUALCROSSING_API_KEY
