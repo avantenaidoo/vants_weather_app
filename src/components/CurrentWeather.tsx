7959b9c1aa20ffc17b39836a0584fb10d5664beb
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-02-03 21:12:46 +0200

    Added state and effect for transition of currentWeather

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -11,20 +11,35 @@ type CurrentWeatherProps = {
 const CurrentWeather = ({ currentWeather, error }: CurrentWeatherProps) => {
 
   console.log('CurrentWeather.tsx file: currentWeather data:', currentWeather);
-  
+
+  const [delayRender, setDelayRender] = useState<boolean>(false); 
   const [showError, setShowError] = useState<boolean>(false);
-  
+
   useEffect(() => {
     if (error) {
       setShowError(true);
 
       const timer = setTimeout(() => {
         setShowError(false);
-      }, 3000)
+      }, 3000);
 
       return () => clearTimeout(timer);
     }
   }, [error]);
+
+
+  useEffect(() => {
+    if (currentWeather) {
+
+      setDelayRender(false);
+
+      const timer = setTimeout(() => {
+        setDelayRender(true);
+      }, 50); 
+
+      return () => clearTimeout(timer); 
+    }
+  }, [currentWeather]); 
 
   if (showError && error) {
     return (
@@ -34,18 +49,13 @@ const CurrentWeather = ({ currentWeather, error }: CurrentWeatherProps) => {
     );
   }
 
-  if (!currentWeather) return null;
 
   let name, country, weatherIcon, weatherDescription, temperature, wind_speed, precip, pressure, humidity, feelslike, displayDate;
 
-
   if (currentWeather && 'currentConditions' in currentWeather) {
-      
-    const { currentConditions, address, resolvedAddress } = currentWeather; 
-  
-    name = address;  
-    country = resolvedAddress.split(',').pop()?.trim(); 
-    
+    const { currentConditions, address, resolvedAddress } = currentWeather;
+    name = address;
+    country = resolvedAddress.split(',').pop()?.trim();
     weatherIcon = `https://github.com/visualcrossing/WeatherIcons/raw/refs/heads/main/SVG/2nd%20Set%20-%20Color/${currentConditions.icon}.svg`;
     weatherDescription = currentConditions.conditions;
     temperature = currentConditions.temp;
@@ -54,19 +64,17 @@ const CurrentWeather = ({ currentWeather, error }: CurrentWeatherProps) => {
     pressure = currentConditions.pressure;
     humidity = currentConditions.humidity;
     feelslike = currentConditions.feelslike;
-  
+
     const localtime = currentConditions.datetime.split(":").slice(0, 2).join(":");
     displayDate = `Today, ${localtime}`;
   }
-      
-  if (currentWeather && 'location' in currentWeather) {
 
+  if (currentWeather && 'location' in currentWeather) {
     const { location, current } = currentWeather;
-    
     weatherIcon = current?.weather_icons?.[0];
     weatherDescription = current?.weather_descriptions?.[0];
-    name = location?.name; 
-    country = location?.country; 
+    name = location?.name;
+    country = location?.country;
     temperature = current?.temperature;
     wind_speed = current?.wind_speed;
     precip = current?.precip;
@@ -75,23 +83,11 @@ const CurrentWeather = ({ currentWeather, error }: CurrentWeatherProps) => {
     feelslike = current?.feelslike;
     displayDate = formatDate(location?.localtime);
   }
-  
 
-  // const { location, current } = weatherData;
-  // const weatherIcon = current?.weather_icons?.[0];
-  // const weatherDescription = current?.weather_descriptions?.[0];
-  // const { name, country, localtime } = location || {}; 
-  // const { temperature, wind_speed, precip, pressure, humidity, feelslike } = current || {}; 
-
-  // const displayDate = formatDate(localtime);
-
-  console.log('CurrentWeather.tsx file: currentWeather data before rendering:', currentWeather);
-  console.log('CurrentWeather.tsx file: weatherData before rendering:', currentWeather);
-
+  if (!currentWeather) return null;
 
   return (
-    <div className={`current-display visible bg-slate-50 bg-opacity-25 p-3 rounded-xl shadow-lg max-w-lg mx-auto mb-3`} role="region" aria-labelledby="CurrentWeather">
-      
+    <div className={`current-display ${delayRender ? 'visible' : ''} bg-slate-50 bg-opacity-25 p-3 rounded-xl shadow-lg max-w-lg mx-auto mb-3`} role="region" aria-labelledby="CurrentWeather">
       <h2 id="CurrentWeather" className="font-bold text-2xl text-center mb-6">{name}</h2>
 
       <div className="flex justify-center items-center space-x-11 mb-6">
