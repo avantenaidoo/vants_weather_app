c9dce187fe47b8a08e3944c08645e3bb6426e9f9
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-02-03 21:18:00 +0200

    added backendUrl

--- backend/server/src/config/config.ts
+++ backend/server/src/config/config.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 export const config = {
     port: process.env.PORT || 3000,
+    backendUrl: process.env.BACKEND_URL || 'http://localhost:3000',
     weatherStackUrl: process.env.WEATHERSTACK_URL,
     weatherStackApiKey: process.env.WEATHERSTACK_API_KEY,
     visualCrossingUrl: process.env.VISUALCROSSING_URL,
